108003e3ea3dda03e34dd110dc8c43f6672314ee
leesb971204 <75251270+leesb971204@users.noreply.github.com>
2021-07-03 17:54:11 +0900

    2021-07-03

---
+++ Java/2914번.java
@@ -0,0 +1,10 @@
+//https://velog.io/@cjhlsb
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+       Scanner sc = new Scanner(System.in);
+       int a = sc.nextInt();
+       int i = sc.nextInt();
+       System.out.println((i-1)*a+1);
+    }
+}
