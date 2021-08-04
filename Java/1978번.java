419931f0d6dd50ef6a66eadf7f3375ebb86a4cf9
leesb971204 <75251270+leesb971204@users.noreply.github.com>
2021-08-04 15:18:45 +0900

    2021-08-04

---
+++ Java/1978번.java
@@ -0,0 +1,25 @@
+//https://velog.io/@cjhlsb
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+    	Scanner sc = new Scanner(System.in);
+    	int num = sc.nextInt();
+    	int cnt = 0;
+    	for(int i=0;i<num;i++)
+    	{
+    		int input = sc.nextInt();
+    		int cnt1 = 0;
+    		for(int k=2;k<=Math.sqrt(input);k++) // 2부터 input/2까지 나누어 떨어지는 수가 없으면 input은 소수
+    		{
+    			
+    			if(input % k == 0)
+    				cnt1++;
+    		}
+    		if(input == 1)
+    			continue;
+    		if(cnt1 == 0)
+    			cnt++;
+    	}
+    	System.out.println(cnt);
+    }
+}
