e850866dfeaef743ca18485bca4d1139d48edf54
leesb971204 <75251270+leesb971204@users.noreply.github.com>
2021-08-23 19:54:49 +0900

    2021-08-23

---
+++ Java/4999번.java
@@ -0,0 +1,10 @@
+//https://velog.io/@cjhlsb
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+    	Scanner sc= new Scanner(System.in);
+    	String input1 = sc.next();
+    	String input2 = sc.next();
+    	System.out.println(input1.length()>=input2.length()?"go":"no");
+    }
+}
