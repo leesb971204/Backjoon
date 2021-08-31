390b7bd5fc114ec3cb452f4d85b79705f0527305
leesb971204 <75251270+leesb971204@users.noreply.github.com>
2021-08-31 18:00:24 +0900

    2021-08-31

---
+++ Java/2475번.java
@@ -0,0 +1,9 @@
+//https://velog.io/@cjhlsb
+import java.util.*;
+public class Main {
+  public static void main(String[] args){
+  	Scanner sc= new Scanner(System.in);
+  	System.out.println((int)(Math.pow(sc.nextDouble(), 2)+Math.pow(sc.nextDouble(), 2)+Math.pow(sc.nextDouble(), 2)
+  	+Math.pow(sc.nextDouble(), 2)+Math.pow(sc.nextDouble(), 2))%10);
+  }
+}	
