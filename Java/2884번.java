b6582ada6b37aee21178bd6fae888d8cfb1743a7
leesb971204 <75251270+leesb971204@users.noreply.github.com>
2021-09-24 21:59:44 +0900

    2021-09-24

---
+++ Java/2884번.java
@@ -0,0 +1,19 @@
+//https://velog.io/@cjhlsb
+import java.util.*;
+public class Main {
+  public static void main(String[] args){
+  	Scanner sc = new Scanner(System.in);
+  	int h = sc.nextInt();
+  	int m = sc.nextInt();
+  	m -= 45;
+  	if(m < 0) {
+  		m = 60 + m;
+  		h--;
+  		if(h < 0) {
+  			h = 23;
+  		}
+  	}
+  	System.out.println(h+" "+m);
+  	
+  }
+}
