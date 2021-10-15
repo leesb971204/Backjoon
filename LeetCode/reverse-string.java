f194745e4aa6289f44d998370bd81ac79319279a
leesb971204 <75251270+leesb971204@users.noreply.github.com>
2021-10-16 00:03:31 +0900

    2021-10-16

---
+++ LeetCode/reverse-string.java
@@ -0,0 +1,12 @@
+//https://velog.io/@cjhlsb
+class Solution {
+    public void reverseString(char[] s) {
+		for(int i=0;i<s.length/2;i++)
+		{
+			char tmp = s[i];
+			s[i] = s[s.length-1-i];
+			s[s.length-1-i] = tmp;
+		}
+        
+    }
+}
