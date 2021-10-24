dcae388cc0155fdbae21645e630132dabc41941a
leesb971204 <75251270+leesb971204@users.noreply.github.com>
2021-10-25 00:37:12 +0900

    2021-10-25

---
+++ programmers/피보나치 수.java
@@ -0,0 +1,13 @@
+//https://velog.io/@cjhlsb
+class Solution {
+    public int solution(int n) {
+        int[] arr = new int[100001];
+        arr[0] = 0;
+        arr[1] = 1;
+        for(int i=2;i<100001;i++)
+        {
+            arr[i] = (arr[i-2] + arr[i-1]) % 1234567;
+        }
+        return arr[n];
+    }
+}
