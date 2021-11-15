6b7889f8232604747c499de4818fdee30672ebb9
leesb971204 <75251270+leesb971204@users.noreply.github.com>
2021-11-15 23:49:25 +0900

    2021-11-15

---
+++ Java/2743번.java
@@ -0,0 +1,8 @@
+//https://velog.io/@cjhlsb
+import java.util.*;
+public class Main{
+	public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+		System.out.println(sc.next().length());
+	}
+} 
