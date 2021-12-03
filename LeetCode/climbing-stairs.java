407a26b755b3fd81c592546d3f461107d95e51be
leesb971204 <75251270+leesb971204@users.noreply.github.com>
2021-12-04 01:04:53 +0900

    2021-12-04

---
+++ LeetCode/climbing-stairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] arr = new int[n+1];
+        arr[0] = 1;
+        arr[1] = 2;
+        for(int i=2;i<n;i++)
+        {
+            arr[i] = arr[i-2] + arr[i-1];
+        }
+        return arr[n-1];
+    }
+}
