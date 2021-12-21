4fcdf0c60888cf0bb49db18e8aac951fe3e5207f
leesb971204 <75251270+leesb971204@users.noreply.github.com>
2021-12-22 00:18:58 +0900

    2021-12-22

--- Java/14659번.java
+++ Java/14659번.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Main{
-   public static void main(String[] args){
+   public static void main(String[] args){ 
 	   Scanner sc = new Scanner(System.in);
 	   int num = sc.nextInt();
 	   int[] arr = new int[num];
