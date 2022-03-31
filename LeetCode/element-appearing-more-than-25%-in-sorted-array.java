5c4b56f7c68e80804c6ac129c94a9e378296bba9
leesb971204 <75251270+leesb971204@users.noreply.github.com>
2022-03-31 18:35:06 +0900

    2022-03-31

---
+++ LeetCode/element-appearing-more-than-25%-in-sorted-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findSpecialInteger(int[] arr) {
+        int n = arr.length/4;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i] == arr[i+n])
+                return arr[i];
+        }
+        return -1;
+    }
+}
